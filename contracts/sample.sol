1d25972eb6277a89e6ea2c97f369384e610b9df8
shotastage <hornet.live.mf@gmail.com>
2022-11-21 10:11:46 +0900

    revise

---
+++ contracts/sample.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SampleContract {
+    uint256 private count = 0;
+    string public data = "Sample Data";
+}
+
--- contracts/todo.sol
+++ contracts/todo.sol
@@ -1,4 +1,21 @@
 //SPDX-License-Identifier: MIT
-pragma solidity
+pragma solidity ^0.8.0;
 
-contract todo { }
+struct TodoEntity {
+    uint id;
+    string label;
+    bool completed;
+}
+
+contract SampleTodo {
+    uint256 private count = 0;
+    string public data = "Sample Data";
+
+    TodoEntity[] public todos;
+
+    function add(string memory label) public {
+        todos.push(TodoEntity(count, label, false));
+        count++;
+    }
+}
+
